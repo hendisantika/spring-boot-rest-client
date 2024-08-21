ebba65868e9238dc64615009ec7a2c182ea8aafa
hendisantika <hendisantika@yahoo.co.id>
2024-08-21 20:20:36 +0700

    Add EmployeeService 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootrestclient/service/EmployeeService.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.springbootrestclient.service;
+
+import id.my.hendisantika.springbootrestclient.repository.EmployeeRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rest-client
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/08/24
+ * Time: 20.20
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@AllArgsConstructor
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+}
