d2eac1f84141a3ce61be72347c82c48d8be3d666
hendisantika <hendisantika@yahoo.co.id>
2024-08-21 20:15:20 +0700

    Add User Entity 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootrestclient/entity/User.java
@@ -0,0 +1,33 @@
+package id.my.hendisantika.springbootrestclient.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rest-client
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/08/24
+ * Time: 20.15
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+}
