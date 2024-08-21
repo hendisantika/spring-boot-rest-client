76f208887c8d4db402b6f513e30ac19f6cbc6851
hendisantika <hendisantika@yahoo.co.id>
2024-08-21 20:26:57 +0700

    Add getUserById in UserController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootrestclient/controller/UserController.java
+++ src/main/java/id/my/hendisantika/springbootrestclient/controller/UserController.java
@@ -5,6 +5,7 @@ import id.my.hendisantika.springbootrestclient.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,5 +32,12 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        return userService.getUserById(id)
+                .map(entity -> ResponseEntity.ok(entity))
+                .orElse(ResponseEntity.notFound().build());
     }
 }
