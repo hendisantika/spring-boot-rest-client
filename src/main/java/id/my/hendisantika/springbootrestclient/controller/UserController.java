124065e664b73e05d3c0899b731dd91dc709b2a8
hendisantika <hendisantika@yahoo.co.id>
2024-08-21 20:27:21 +0700

    Add updateUser in UserController 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootrestclient/controller/UserController.java
+++ src/main/java/id/my/hendisantika/springbootrestclient/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,6 +46,11 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<User> createUser(@RequestBody User user) {
+        return ResponseEntity.ok(userService.saveUser(user));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
         return ResponseEntity.ok(userService.saveUser(user));
     }
 }
