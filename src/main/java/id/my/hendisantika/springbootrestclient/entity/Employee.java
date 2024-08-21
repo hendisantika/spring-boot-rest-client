b68b0d696151ba60254ab20917931a7da2bf8a42
hendisantika <hendisantika@yahoo.co.id>
2024-08-21 20:13:30 +0700

    Add Employee Entity 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootrestclient/entity/Employee.java
@@ -0,0 +1,44 @@
+package id.my.hendisantika.springbootrestclient.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rest-client
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/08/24
+ * Time: 20.13
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}
