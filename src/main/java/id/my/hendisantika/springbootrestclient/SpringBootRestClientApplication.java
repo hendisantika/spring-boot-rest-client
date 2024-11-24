b68564dfcd93ac6209ebb051813fa807f2d77074
hendisantika <hendisantika@yahoo.co.id>
2024-11-24 08:16:28 +0700

    Add Spring Doc API🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/springbootrestclient/SpringBootRestClientApplication.java
+++ src/main/java/id/my/hendisantika/springbootrestclient/SpringBootRestClientApplication.java
@@ -1,7 +1,12 @@
 package id.my.hendisantika.springbootrestclient;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootRestClientApplication {
@@ -10,4 +15,15 @@ public class SpringBootRestClientApplication {
         SpringApplication.run(SpringBootRestClientApplication.class, args);
     }
 
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${application-description}") String appDescription, @Value("${application" +
+            "-version}") String appVersion) {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("REST Client Sample application API")
+                        .version(appVersion)
+                        .description(appDescription)
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
 }
